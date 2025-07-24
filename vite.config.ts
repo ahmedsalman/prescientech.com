b3671643d3187735c4886760b9b648f367358e22
Ahmed Salman Noor <ahmed.salman.noor@gmail.com>
2025-07-24 22:44:39 +0500

    fix Blocked request. This ho

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
   },
   preview: {
-    host: "::",
+    host: "0.0.0.0",
     port: 4173,
   },
   plugins: [react()],
